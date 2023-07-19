7bdf9d60dd215fb6b999eee7024218fbb9fe722e
Volodymyr <yeikovychvolodymyrwork@gmail.com>
2023-07-19 14:23:55 +0200

    Added builder pattern.

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,21 +5,26 @@ import java.lang.reflect.Modifier;
 
 public class Main {
     public static void main(String[] args) throws IllegalAccessException {
-        Person him = new Person("Vale", "Chambers", 17);
+        Person him = new Person.PersonBuilder("Vale", "Chambers", 17).build();
 
         printVariablesInfo(him);
+
+        System.out.println();
+
+        Person other = new Person.PersonBuilder("Kyle", "Hustles", 17).setMiddleName("Jan").build();
+
+        printVariablesInfo(other);
     }
 
-    public static<T> void printVariablesInfo(T t) throws IllegalAccessException {
+    public static void printVariablesInfo(Object t) throws IllegalAccessException {
         Field[] values = t.getClass().getDeclaredFields();
         for (Field field : values) {
             field.setAccessible(true);
-            System.out.printf("Access Modifier:{%s}, Var Type:{%s}, Var Name:{%s}, Value:{%s}; %n", extractAccessModifier(field), field.getGenericType(), field.getName(), field.get(t));
+            String accessMod = Modifier.toString(field.getModifiers());
+            System.out.printf("Access Modifier:{%s}, Var Type:{%s}, Var Name:{%s}, Value:{%s}; %n", accessMod, field.getGenericType(), field.getName(), field.get(t));
             field.setAccessible(false);
         }
     }
 
-    private static String extractAccessModifier(Field field) {
-        return Modifier.toString(field.getModifiers());
-    }
+
 }
--- src/main/java/org/example/Person.java
+++ src/main/java/org/example/Person.java
@@ -1,13 +1,69 @@
 package org.example;
 
-public class Person {
-    private String name;
-    private String surname;
-    private int age;
+import org.jetbrains.annotations.Nullable;
 
-    public Person(String name, String surname, int age) {
-        this.name = name;
-        this.surname = surname;
-        this.age = age;
+import java.util.Objects;
+
+public class Person {
+    private final String name;
+    @Nullable
+    private final String middleName;
+    private final String surname;
+    private final int age;
+
+    public Person(PersonBuilder personBuilder) {
+        this.name = personBuilder.name;
+        this.middleName = personBuilder.middleName;
+        this.surname = personBuilder.surname;
+        this.age = personBuilder.age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Person person = (Person) o;
+        return age == person.age && Objects.equals(name, person.name) && Objects.equals(surname, person.surname);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, surname, age);
+    }
+
+    public static class PersonBuilder{
+        private final String name;
+        @Nullable
+        private String middleName;
+        private final String surname;
+        private final int age;
+
+        public PersonBuilder(String name, String surname, int age) {
+            this.name = name.length() >= 2 ? name : "John";
+            this.middleName = "Junior";
+            this.surname = surname.length() >= 2 ? surname : "Doe";
+            this.age = age > 0 ? age : 18;
+        }
+
+        public PersonBuilder setMiddleName(String middleName) {
+            this.middleName = middleName.length() >= 2 ? middleName : "Junior";
+            return this;
+        }
+
+        public Person build() {
+            return new Person(this);
+        }
     }
 }
